afe918b07685d412b43682eb0da85cb9f3f388b5
Shifa Alam <shifa.alam@rite.com.bd>
2022-01-03 19:15:35 +0600

    updated

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxDeviceIP = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.deviceInfoTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbxDeviceIP
+            // 
+            this.tbxDeviceIP.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxDeviceIP.Location = new System.Drawing.Point(123, 44);
+            this.tbxDeviceIP.Name = "tbxDeviceIP";
+            this.tbxDeviceIP.Size = new System.Drawing.Size(99, 20);
+            this.tbxDeviceIP.TabIndex = 1;
+            this.tbxDeviceIP.Text = "192.168.1.201";
+            this.tbxDeviceIP.TextChanged += new System.EventHandler(this.tbxDeviceIP_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(228, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // deviceInfoTextBox
+            // 
+            this.deviceInfoTextBox.Location = new System.Drawing.Point(123, 70);
+            this.deviceInfoTextBox.Name = "deviceInfoTextBox";
+            this.deviceInfoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.deviceInfoTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.deviceInfoTextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbxDeviceIP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxDeviceIP;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox deviceInfoTextBox;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BioMetrixCore;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        DeviceManipulator manipulator = new DeviceManipulator();
+        public ZkemClient objZkeeper;
+        private bool isDeviceConnected = false;
+
+        public bool IsDeviceConnected
+        {
+            get { return isDeviceConnected; }
+            set
+            {
+                isDeviceConnected = value;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void RaiseDeviceEvent(object sender, string actionType)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ipAddress = tbxDeviceIP.Text.Trim();
+            int portNumber = 4370;
+
+            objZkeeper = new ZkemClient(RaiseDeviceEvent);
+            IsDeviceConnected = objZkeeper.Connect_Net(ipAddress, portNumber);
+            if (IsDeviceConnected)
+            {
+                string deviceInfo = manipulator.FetchDeviceInfo(objZkeeper, int.Parse("1"));
+                deviceInfoTextBox.Text = deviceInfo;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbxDeviceIP_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
